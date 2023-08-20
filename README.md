9896f34db543db6a902b84e53aad0ee10cf82981
Eugenio Petullà <eugeniopetulla@yahoo.it>
2023-08-20 21:07:10 +0200

    First release

--- README.md
+++ README.md
@@ -1,26 +1,20 @@
-# My plugin
+# PurrCast - Podcast Finder
 
-[![awesome plugin](https://custom-icon-badges.demolab.com/static/v1?label=&message=awesome+plugin&color=383938&style=for-the-badge&logo=cheshire_cat_ai)](https://)  
-[![Awesome plugin](https://custom-icon-badges.demolab.com/static/v1?label=&message=Awesome+plugin&color=000000&style=for-the-badge&logo=cheshire_cat_ai)](https://)  
 [![awesome plugin](https://custom-icon-badges.demolab.com/static/v1?label=&message=awesome+plugin&color=F4F4F5&style=for-the-badge&logo=cheshire_cat_black)](https://)
 
-Write here all the useful information about your plugin.
-
-This repository is the template to automate the release of official Cheshire Cat AI plugins. 
+Search for public podcast shows and episodes on Spreaker using The Cheshire Cat AI.
 
 ## Usage
 
-1. Create a new repository clicking on the `Use this template` button.
-2. Clone your new repo directly in the Cat's `plugins` folder.
-3. Run the `setup.py` script:
-```bash
-python setup.py
-```
-The script will prompt you to write the name of your plugin and make an initial setup setting the name in the files.
+### Find a Podcast Show
 
-4. Start developing!
+Ask the Cat to find a podcast.
+   
+E.g. "Find some podcasts about Artificial Intelligence"
 
-> **Important**
-> A new release of your plugin is triggered every time you set a new `version` in the `plugin.json` file.
-> Please, remember to set it correctly every time to want to release an update.
+### Find a Podcast Episode
+
+Ask the Cat to find an episode of a podcast.
+
+E.g. "Search for a podcast episode that talks about Wild Animals"
 
---
+++ purrcast_podcast_finder.py
@@ -0,0 +1,77 @@
+from cat.mad_hatter.decorators import tool
+import requests
+
+
+SPREAKER_API_BASE_URL = "https://api.spreaker.com/v2"
+
+
+def get_author_info(author_id):
+    endpoint = f"{SPREAKER_API_BASE_URL}/users/{author_id}"
+    response = requests.get(endpoint)
+
+    if response.status_code == 200:
+        author_info = response.json()["response"]["user"]
+        return author_info
+    else:
+        return None
+
+
+@tool
+def search_shows(query, cat):
+    """
+    Search for a podcast (a show). The input is a query.
+
+    For example, search for a podcast about curious animals.
+    Query -> 'curious animals'
+    """
+    endpoint = f"{SPREAKER_API_BASE_URL}/search"
+    params = {
+        "type": "shows",
+        "q": query,
+        "limit": 10,
+    }
+    response = requests.get(endpoint, params=params)
+
+    if response.status_code == 200:
+        results = response.json()["response"]["items"]
+        shows_info = []
+        for show in results:
+            author_id = show["author_id"]
+            author_info = get_author_info(author_id)
+            author_name = author_info["fullname"] if author_info else "Unknown Author"
+            shows_info.append(f"Show Name: {show['title']} by {author_name}\nLink: {show['site_url']}")
+        return "\n".join(shows_info)
+    else:
+        return f"Error: {response.status_code}"
+
+
+@tool
+def search_episodes(query, cat):
+    """
+    Search for a podcast episode. The input is a query.
+
+    For example, search for a podcast episode about Alice in wonderland.
+    Query -> 'alice in wonderland'
+    """
+    endpoint = f"{SPREAKER_API_BASE_URL}/search"
+    params = {
+        "type": "episodes",
+        "q": query,
+        "limit": 10,
+    }
+    response = requests.get(endpoint, params=params)
+
+    if response.status_code == 200:
+        results = response.json()["response"]["items"]
+        episodes_info = []
+        for episode in results:
+            show_title = episode["show"]["title"]
+            episode_title = episode["title"]
+            author_id = episode["show"]["author_id"]
+            author_info = get_author_info(author_id)
+            author_name = author_info["fullname"] if author_info else "Unknown Author"
+            episodes_info.append(
+                f"Show Title: {show_title}\nEpisode Title: {episode_title} by {author_name}\nLink: {episode['site_url']}")
+        return "\n".join(episodes_info)
+    else:
+        return f"Error: {response.status_code}"
